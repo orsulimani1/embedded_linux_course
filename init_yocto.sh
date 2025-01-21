9cc9cc723ecd002f69f4fc453c9b448d975be478
Or Sulimani <orsulimani@gmail.com>
2025-01-21 08:40:09 +0200

    Add build directory without its contents

--- init_yocto.sh
+++ init_yocto.sh
@@ -9,7 +9,7 @@ if [ -d "$DIR" ]; then
     cd "$DIR" || { echo "Failed to change directory to $DIR"; exit 1; }
 
     # Source the oe-init-build-env script with the specified build directory
-    source oe-init-build-env ../../build/
+    source oe-init-build-env ../../yocto_build/
 
 else
     echo "The directory '$DIR' run 'git submodule update --init --recursive"
